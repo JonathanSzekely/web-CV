37dcce04417b5582bf3064262480094120274374
jonathanszekely3@gmail.com <jonathanszekely3@gmail.com>
2025-01-19 21:49:28 +0200

    - Initial commit - setup, added Tailwind 3, minimal project structure

---
+++ src/app/constants/skills.ts
@@ -0,0 +1,7 @@
+export let skills = new Map<string, Array<string>>([
+  ["CSS, SCSS, SASS", ['Tailwind 3-2', 'Bootstrap 4-3', 'Material (Angular)']],
+  ["HTML", ['Bootstrap', 'Google AM']],
+  ["UI & UX", ['Figma', 'Photoshop', 'Illustrator', 'Moqups']],
+  ["JavaScript", ['Typescript', 'Angular', 'RxJS', 'jQuery', 'AngularJS']],
+  ["Dart", ['Flutter']],
+]);
---
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+import { skills } from '../../constants/skills';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.scss'
+})
+
+export class SidebarComponent {
+  skills = skills;
+}
