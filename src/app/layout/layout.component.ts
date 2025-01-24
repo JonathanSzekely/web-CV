07f3d55b3a028aaa69e3e53ec36b79724824d653
jonathanszekely3@gmail.com <jonathanszekely3@gmail.com>
2025-01-24 17:24:50 +0200

    - Updated structure - Updated Header - Added Skills sidebar - Added Profile and Contact info

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from "./sidebar/sidebar.component";
+import { ContentComponent } from "./content/content.component";
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderComponent, SidebarComponent],
+  imports: [HeaderComponent, SidebarComponent, ContentComponent, RouterOutlet, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
